938d7a1f879d9c4c70e4bb4578ea125bf3acd316
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2019-04-28 16:33:47 -0400

    last minute

--- resources/views/config.php
+++ resources/views/config.php
@@ -3,8 +3,8 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_PASSWORD', 'rootroot');
+define('DB_NAME', 'mccs');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,115 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@include ('layouts.header')
+<!-- <title>Marist College Computer Society</title> -->
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<div class="position-relative overflow-hidden text-center row align-items-center hero" id="particles-js">
+  <div class="container">
+    <h1 class="display-3 font-weight-normal" id="hero-heading">Marist College Computer Society</h1>
+    <p class="lead font-weight-normal" id="hero-subheading">Hacking since 19xx</p>
+  </div>
+</div>
+<div class="container-fluid p-4 mission-section">
+  <h2 class="text-center">Our Mission</h2>
+  <div class="row container">
+    <div class="col p-4">
+      <p class="secondary-text">
+        Marist College Computer Society exists to bridge the gap between Industry Career Expectations and Academia.
+      </p>
+      <p class="secondary-text">
+        We strive to provide real-world experience for members of varying technical proficiencies in the fields of Computer Science and Information Technology and Systems.
+      </p>
+      <p class="secondary-text">
+        We accomplish this through engaging discussions, hands-on learning, and problem-solving competitions.
+      </p>
+    </div>
+    <div class="col p-4">
+        <img class="p-4 mx-auto float-right"src="/images/code-illustration.png" alt="">
+    </div>
+  </div>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+<div class="container-fluid p-4 club-information">
+  <h2 class="text-center">Club Information</h2>
+  <div class="row">
+    <div class="col p-4">
+      <p>
+        Marist College Computer Society is a college for Marist students who are interested and passionate about computer science and engineering.
+      </p>
+      <p>
+        We meet every <strong>Wednesday</strong> at <strong>9.15pm</strong> in <strong>Donnelly 225</strong>
+      </p>
+    </div>
+    <div class="col p-4">
+      <p class="text-left">Calendar Events</p>
+      <ul>
+        <li>2/26/19 - <strong>Code Review Day</strong></li>
+        <li>2/26/19 - <strong>Code Review Day</strong></li>
+        <li>2/26/19 - <strong>Code Review Day</strong></li>
+        <li>2/26/19 - <strong>Code Review Day</strong></li>
+      </ul>
+    </div>
+  </div>
+</div>
 
-            .full-height {
-                height: 100vh;
-            }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<div class="container-fluid p-4 executive-board">
+  <h2 class="text-center pb-4">Executive Board</h2>
+    <div class="row">
+      <div class="container">
+        <div class="card-deck pb-4">
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
             </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
+            </div>
+          </div>
         </div>
-    </body>
-</html>
+        <div class="card-deck pb-4">
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
+            </div>
+          </div>
+          <div class="card">
+            <img class="card-img-top" src="/images/profile-1.jpg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">John Smith</h5>
+              <p class="card-text">President</p>
+              <small class="text-muted">Sophmore, C.S. Major</small>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+
+@include ('layouts.footer')
